e3e3a64f3da3f2a9d5b8c434be1ce3f3cfcf6792
Klaas <klaas.neyens@gmail.com>
2023-04-23 20:18:54 +0200

    changed fetch link

--- js/randemon.js
+++ js/randemon.js
@@ -39,7 +39,7 @@ class RandemonMap {
 }
 
 
-const imageUrl = "http://127.0.0.1:8000";
+const imageUrl = "https://randemon.azurewebsites.net/";
 const MAX_MAPS = 5;
 let previousMaps = [];
 let keepMaps = [];
